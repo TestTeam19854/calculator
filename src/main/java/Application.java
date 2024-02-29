9b75a0a429e7c2dd573529b809ab31b9ea22b234
jina <iwbyl1220@gmail.com>
2024-02-29 17:39:10 +0900

    Plus_minus

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -2,17 +2,8 @@ import java.util.Calendar;
 import java.util.Scanner;
 
 public class Application {
-    public static void main(String[] args) {
-
-        Scanner sc = new Scanner(System.in);
-        System.out.println("정수 하나를 입력해주세요.");
-
-        int a = sc.nextInt();
-        int b = sc.nextInt();
 
 
-
-    }
     public static class Calculator {
 
         public void plus(int a, int b) {
